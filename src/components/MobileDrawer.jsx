36665f7a5dd6e9d5c128e486992ae7f0ce30521c
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2023-05-15 12:04:30 +0700

    fix error on deploy. uppercase lowercase mac

--- src/components/MobileDrawer.jsx
+++ src/components/MobileDrawer.jsx
@@ -1,36 +1,40 @@
 import {
-  useDisclosure,
-  Flex,
-  Button,
-  VStack,
-  Icon,
-  Link as ChakraLink,
+    useDisclosure,
+    Flex,
+    Button,
+    VStack,
+    Icon,
+    Link as ChakraLink,
 } from "@chakra-ui/react";
-import CustomDrawer from "./customDrawer";
+import CustomDrawer from "./CustomDrawer";
 import { IoMdMenu } from "react-icons/io";
 import React from "react";
 import pages from "../helper/routes";
 
 export default function MobileDrawer() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const btnRef = React.useRef();
 
-  //your path to pages
+    //your path to pages
 
-  return (
-    <Flex display={{ base: "flex", md: "none" }}>
-      <Button ref={btnRef} onClick={onOpen}>
-        <IoMdMenu size="26px" />
-      </Button>
-      <CustomDrawer isOpen={isOpen} onClose={onClose} finalFocusRef={btnRef}>
-        <VStack alignItems="left">
-          {pages.map((page, i) => (
-            <ChakraLink key={i} href={page.path}>
-              <Button variant="text">{page.name}</Button>
-            </ChakraLink>
-          ))}
-        </VStack>
-      </CustomDrawer>
-    </Flex>
-  );
+    return (
+        <Flex display={{ base: "flex", md: "none" }}>
+            <Button ref={btnRef} onClick={onOpen}>
+                <IoMdMenu size="26px" />
+            </Button>
+            <CustomDrawer
+                isOpen={isOpen}
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <VStack alignItems="left">
+                    {pages.map((page, i) => (
+                        <ChakraLink key={i} href={page.path}>
+                            <Button variant="text">{page.name}</Button>
+                        </ChakraLink>
+                    ))}
+                </VStack>
+            </CustomDrawer>
+        </Flex>
+    );
 }
